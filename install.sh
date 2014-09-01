3fd9542ef9c3a3390ee493dc35a391f7ad46a6f2
soheil <ss2@elec-lab.tk>
2014-09-01 20:12:44 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-nohup sh -c "python $OPENSHIFT_HOMEDIR/app-root/runtime/repo/trash/t2/PythonProxy.py" >    $OPENSHIFT_LOG_DIR/proxy_run.log 2>&1 &
- tail -f $OPENSHIFT_LOG_DIR/proxy_run.log
+git clone https://github.com/ss2222/openshift-proxy.git
+cd openshift-proxy
+
+#nohup sh -c "python $OPENSHIFT_HOMEDIR/app-root/runtime/repo/trash/t2/PythonProxy.py" >    $OPENSHIFT_LOG_DIR/proxy_run.log 2>&1 &
+nohup sh -c "python PythonProxy.py" >    $OPENSHIFT_LOG_DIR/proxy_run.log 2>&1 &
+tail -f $OPENSHIFT_LOG_DIR/proxy_run.log
 #stop
 #kill -9 `lsof -t -i :15000`
